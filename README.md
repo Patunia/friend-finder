cb3f6d8ca65162f5bd3763f8e8994e8bff8cc2d1
Patricia Borowiec <pborowiec@alumni.nd.edu>
2018-06-10 14:23:48 -0500

    Update!!!

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # friend-finder
+
+a dating app that will take in results from users' surveys, then compare their answers with those from other users. Then app will display the name and picture of the user with the best overall match!
+
+## Install
+
+- express
+- node
+
+## Author
+
+Patricia Joann Borowiec
---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsList = [
+	{
+	  name: "Ray Ron",
+	  photo: "https://www.drodd.com/images8/1%20RAY85742.jpg",
+	  preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+	},
+
+	{
+	  name: "Link Wurther",
+	  photo: "http://cyber-breeze.com/wp-content/uploads/2016/11/001-12-scheming-black-cat-600071.jpg",
+	  preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
+	},
+
+	{
+	  name: "Sherma Lancelot",
+	  photo: "http://thedreamwithinpictures.com/wp-content/uploads/2014/03/104f3__headshots2-620x420.jpg",
+	  preferences: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+	},
+];
+
+module.exports = friendsList;
